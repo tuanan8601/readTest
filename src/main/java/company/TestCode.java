f4ab085d3c50499c95aa6c6affe9d03b1d7cf4b5
admin <tuanan8601@gmail.com>
2021-09-17 18:16:09 +0700

    Read the Objective Test write to database

--- src/main/java/company/TestCode.java
+++ src/main/java/company/TestCode.java
@@ -20,38 +20,52 @@ public class TestCode {
         index.add(0L);
         return Collections.max(index);
     }
-    public static void addQuestionList(String pathRead, Jedis jedis, ObjectiveTest objectiveTest){
+    public static void addQuestionList(String pathRead, Jedis jedis, ObjectiveTest objectiveTest, Subject subject){
         List<Question> questionList = ReadTest.readTest(pathRead);
         long i=getMaxIndex("question");
         Map<String,String> map;
 
-        map = new HashMap<>();
-        map.put("id",String.valueOf(objectiveTest.getId()));
-        map.put("testname",objectiveTest.getTestName());
-        if(objectiveTest.getPoster()!=null)
-            map.put("poster",objectiveTest.getPoster());
-        jedis.hmset("objectivetest:"+objectiveTest.getId(),map);
-        jedis.sadd("objectivetestindex:"+objectiveTest.getTestName(),""+objectiveTest.getId());
-
-        for (Question q:questionList) {
-            i++;
-            map=new HashMap<>();
-            map.put("title",q.getTitle());
-            map.put("solution",q.getSolution());
-            map.put("solutionHead",String.valueOf(q.getSolutionHead()));
-            if(q.getFeedback()!=null)
-                map.put("feedback",q.getFeedback());
-            if(q.getImage()!=null)
-                map.put("image",q.getFeedback());
-            jedis.hmset("question:"+i,map);
-            for (Answer answer:q.getAnswers()) {
-                jedis.lpush("answer:question:"+i,answer.getAnswer());
-            }
-            jedis.sadd("questionset:objectivetest:"+objectiveTest.getId(),String.valueOf(i));
+        if(!jedis.hexists("subjectindex",subject.getName())) {
+            map = new HashMap<>();
+            map.put("id", String.valueOf(subject.getId()));
+            map.put("name", subject.getName());
+            if (subject.getPoster() != null)
+                map.put("poster", subject.getPoster());
+            map.put("type", subject.getType());
+            jedis.hmset("subject:" + subject.getId(), map);
+            jedis.hsetnx("subjectindex", subject.getName(), "" + subject.getId());
+            jedis.sadd("subjecttypeindex:" + subject.getType(), "" + subject.getId());
+            jedis.sadd("objectivetestset:" + subject.getId(), "" + objectiveTest.getId());
         }
-    }
-    public static void addOT2Subject(Jedis jedis, Subject subject, ObjectiveTest objectiveTest){
+        else System.out.println("Môn học đã có");
 
+        if(!jedis.hexists("objectivetestindex",objectiveTest.getTestName())) {
+            map = new HashMap<>();
+            map.put("id", String.valueOf(objectiveTest.getId()));
+            map.put("testname", objectiveTest.getTestName());
+            if (objectiveTest.getPoster() != null)
+                map.put("poster", objectiveTest.getPoster());
+            jedis.hmset("objectivetest:" + objectiveTest.getId(), map);
+            jedis.hsetnx("objectivetestindex", objectiveTest.getTestName(), "" + objectiveTest.getId());
+
+            for (Question q : questionList) {
+                i++;
+                map = new HashMap<>();
+                map.put("title", q.getTitle());
+                map.put("solution", q.getSolution());
+                map.put("solutionHead", String.valueOf(q.getSolutionHead()));
+                if (q.getFeedback() != null)
+                    map.put("feedback", q.getFeedback());
+                if (q.getImage() != null)
+                    map.put("image", q.getFeedback());
+                jedis.hmset("question:" + i, map);
+                for (Answer answer : q.getAnswers()) {
+                    jedis.lpush("answer:question:" + i, answer.getAnswer());
+                }
+                jedis.sadd("questionset:objectivetest:" + objectiveTest.getId(), String.valueOf(i));
+            }
+        }
+        else System.out.println("Bài trắc nghiệm đã có");
     }
     public static void readListObject (String objectname,Jedis jedis){
         HashSet<String> list = (HashSet<String>) jedis.keys(objectname+":*");
@@ -84,7 +98,16 @@ public class TestCode {
     }
     public static void deleteAllObject(String objectname,Jedis jedis){
         HashSet<String> list = (HashSet<String>) jedis.keys(objectname+":*");
-        System.out.println(list);
+        System.out.println("deleted:"+list);
+        Iterator<String> keys = list.iterator();
+        while (keys.hasNext()){
+            String key = keys.next();
+            jedis.del(key);
+        }
+    }
+    public static void deleteIndex(String objectname,Jedis jedis){
+        HashSet<String> list = (HashSet<String>) jedis.keys(objectname+"index");
+        System.out.println("deleted:"+list);
         Iterator<String> keys = list.iterator();
         while (keys.hasNext()){
             String key = keys.next();
@@ -100,23 +123,29 @@ public class TestCode {
         Jedis jedis = new Jedis("localhost");
         System.out.println("Connection to server sucessfully");
 
+
+
 //        ObjectiveTest objectiveTest = new ObjectiveTest();
 //        objectiveTest.setId(getMaxIndex("objectivetest")+1);
 //        objectiveTest.setTestName(readname);
-//        addQuestionList(pathRead,jedis,objectiveTest);
+//
+//        Subject subject = new Subject();
+//        subject.setId(getMaxIndex("subject")+1);
+//        subject.setName("Chủ nghĩa xã hội khoa học");
+//        subject.setPoster("");
+//        subject.setType("đại cương");
+//
+//        addQuestionList(pathRead,jedis,objectiveTest,subject);
 
-        Subject subject = new Subject();
-        subject.setId(getMaxIndex("subject")+1);
-        subject.setName("Chủ nghĩa xã hội khoa học");
-        subject.setPoster("");
-        subject.setType("đại cương");
 
 
 
+//        System.out.println(jedis.hgetAll("subjectindex"));
+//        deleteIndex("*",jedis);
 //        deleteAllObject("*",jedis);
 //        readHashObject("question",jedis);
-//        readListObject("answer:question",jedis);
-//        readSetObject("objectivetestindex",jedis);
+//        readListObject("question",jedis);
+//        readSetObject("subjectindex",jedis);
 //        System.out.println(getMaxIndex("answer"));
     }
 } 
