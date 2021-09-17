68b2f3a45bb209383e705af4e35256c8a4fa8b38
admin <tuanan8601@gmail.com>
2021-09-17 09:49:42 +0700

    Read the Objective Test write to database

--- src/main/java/Test.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-import java.util.List;
-
-public class Test {
-    public static void main(String[] args) {
-        String s = "Toi . hom . nay";
-        System.out.println(s.split(".")[-1]);
-    }
-}
