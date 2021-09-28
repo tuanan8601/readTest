12f57d8d42289ca0bc395c90d91771eefc57e8b9
admin <tuanan8601@gmail.com>
2021-09-28 20:00:11 +0700

    Read the Objective Test write to database

--- src/main/java/company/RedisCommentTest.java
+++ src/main/java/company/RedisCommentTest.java
@@ -4,9 +4,7 @@ import company.entity.Comment;
 import redis.clients.jedis.Jedis;
 
 import java.time.Instant;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class RedisCommentTest {
     public static void addComment(Comment comment,Jedis jedis){
@@ -19,18 +17,29 @@ public class RedisCommentTest {
         map.put("date",String.valueOf(comment.getDate().getTime()));
         System.out.println(map);
         jedis.hmset("comment:"+comment.getId(),map);
+        jedis.zadd("commentzset:objectivetest:"+comment.getObjectiveTest_id(),comment.getDate().getTime(),""+comment.getId());
     }
+//    public static void deleteComment(String id,Jedis jedis){
+//        Iterator<String> keys = jedis.keys("comment:"+id).iterator();
+//        while(keys.hasNext()){
+//            String key = keys.next();
+//            System.out.println(key);
+//            jedis.del(key);
+//        }
+//    }
     public static void main(String[] args) {
         Jedis jedis = new Jedis("localhost");
         Comment comment = new Comment();
         comment.setEmail("tuanan8601@gmail.com");
         comment.setId(RedisDB.getMaxIndex("comment",jedis)+1);
         comment.setName("Tuấn An Nguyễn");
-        comment.setText("Wow! Amazing");
-        comment.setObjectiveTest_id(8);
+        comment.setText("Thank you");
+        comment.setObjectiveTest_id(4);
         comment.setDate(new Date());
         System.out.println(comment);
 //        addComment(comment,jedis);
-        RedisDB.readHashObject("comment",jedis);
+//        RedisDB.readHashObject("comment",jedis);
+        RedisDB.readZSetObject("commentzset",jedis);
+//        deleteComment("*",jedis);
     }
 }
